ef6d51bd09ff4249a49254224192f2a4a7fb396c
lnwkit007 <164681756+lnwkit007@users.noreply.github.com>
2026-05-21 16:02:09 +0700

    Update function createShowtime.

--- app/Http/Controllers/ShowtimeController.php
+++ app/Http/Controllers/ShowtimeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Showtime;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -9,6 +10,24 @@ class ShowtimeController extends Controller
 {
     public function createShowtime(Request $request) : JsonResponse
     {
-        $request->validate([]);
+        $request->validate([
+            'movie_id' => 'required|exists:movies,id',      
+            'theater_id' => 'required|exists:theaters,id', 
+            'start_time' => 'required|date_format:Y-m-d H:i:s', 
+            'base_price' => 'required|numeric|min:0', 
+        ]);
+
+        $showtime = Showtime::create([
+            'movie_id' => $request->movie_id,
+            'theater_id' => $request->theater_id,
+            'start_time' => $request->start_time,
+            'base_price' => $request->base_price
+        ]);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Create showtime successfully',
+            'data' => $showtime
+        ], 201);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,24 @@
 <?php
 
 use App\Http\Controllers\MovieController;
+use App\Http\Controllers\ShowtimeController;
 use App\Http\Controllers\TicketController;
 use Illuminate\Support\Facades\Route;
+
+//////////////////////////////////// Users ////////////////////////////////////
 
 // Movie
 Route::get('/movies', [MovieController::class, 'getMovies']);
 Route::get('/movie/{id}', [MovieController::class, 'getMovie']);
 
+// Ticket
+Route::post('/ticket/booking', [TicketController::class, 'bookingTicket']);
+
+//////////////////////////////////// Admin ////////////////////////////////////
+
+// movie
 Route::post('/admin/movie', [MovieController::class, 'createMovie']);
 Route::patch('/admin/movie/{id}', [MovieController::class, 'updateMovie']);
 
-// Ticket
-Route::post('/ticket/booking', [TicketController::class, 'bookingTicket']);
+// showtime
+Route::post('/admin/showtime', [ShowtimeController::class, 'createShowtime']);
