1f053637a43a4039fbf215f6e6ff6c4dcbcf51bc
lnwkit007 <164681756+lnwkit007@users.noreply.github.com>
2026-05-23 03:14:32 +0700

    Update add jsonresponse.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function register (Request $request) {
+    public function register (Request $request) : JsonResponse {
         $request->validate([
             'user_name' => 'required|string|max:255',
             'user_email' => 'required|string|email|max:255|unique:users,user_email',
@@ -23,14 +24,14 @@ class AuthController extends Controller
 
         $token = $user->createToken('auth_token')->plainTextToken;
 
-        return response()->json([
-            'status' => 'success',
-            'message' => 'Register successfully',
-            'data' => [
-                'user' => $user,
-                'token' => $token,
-                'token_type' => 'Bearer'
-            ]
-        ], 201);
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Register successfully',
+                'data' => [
+                    'user' => $user,
+                    'token' => $token,
+                    'token_type' => 'Bearer'
+                ]
+            ], 201);    
     }
 }
