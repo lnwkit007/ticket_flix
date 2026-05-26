e7a87bb0e9979cce741ed4daacbb81755fa46541
lnwkit007 <164681756+lnwkit007@users.noreply.github.com>
2026-05-27 00:09:58 +0700

    Update api auth:sanctum.

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 // Auth
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sunctum');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 //////////////////////////////////// Users ////////////////////////////////////
 
@@ -18,14 +18,14 @@ Route::get('/movies', [MovieController::class, 'getMovies']);
 Route::get('/movie/{id}', [MovieController::class, 'getMovie']);
 
 // Ticket
-Route::post('/ticket/booking', [TicketController::class, 'bookingTicket'])->middleware('auth:sunctum');
-Route::get('/users/{userId}/tickets', [TicketController::class, 'getMyBookingHistory'])->middleware('auth:sunctum');
+Route::post('/ticket/booking', [TicketController::class, 'bookingTicket'])->middleware('auth:sanctum');
+Route::get('/users/{userId}/tickets', [TicketController::class, 'getMyBookingHistory'])->middleware('auth:sanctum');
 
 //////////////////////////////////// Admin ////////////////////////////////////
 
 // movie
-Route::post('/admin/movie', [MovieController::class, 'createMovie'])->middleware('auth:sunctum');
-Route::patch('/admin/movie/{id}', [MovieController::class, 'updateMovie'])->middleware('auth:sunctum');
+Route::post('/admin/movie', [MovieController::class, 'createMovie'])->middleware('auth:sanctum');
+Route::patch('/admin/movie/{id}', [MovieController::class, 'updateMovie'])->middleware('auth:sanctum');
 
 // showtime
-Route::post('/admin/showtime', [ShowtimeController::class, 'createShowtime'])->middleware('auth:sunctum');
+Route::post('/admin/showtime', [ShowtimeController::class, 'createShowtime'])->middleware('auth:sanctum');
