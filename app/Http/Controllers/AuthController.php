2f5f50209e1ebae27a6ced8a80a43f2b48687cce
lnwkit007 <164681756+lnwkit007@users.noreply.github.com>
2026-06-30 15:56:44 +0700

    Update try-catch in function register.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 
 class AuthController extends Controller
 {
@@ -18,23 +19,32 @@ class AuthController extends Controller
             // password_confirmation 
         ]);
 
-        $user = User::create([
-            'user_name' => $request->user_name,
-            'user_email' => $request->user_email,
-            'password' => Hash::make($request->password)
-        ]);
+        try {
+            $user = User::create([
+                'user_name' => $request->user_name,
+                'user_email' => $request->user_email,
+                'password' => Hash::make($request->password)
+            ]);
 
-        $token = $user->createToken('auth_token')->plainTextToken;
+            $token = $user->createToken('auth_token')->plainTextToken;
 
-        return response()->json([
-            'status' => 'success',
-            'message' => 'User registered successfully.',
-            'data' => [
-                'user' => $user,
-                'token' => $token,
-                'token_type' => 'Bearer'
-            ]
-        ], 201);
+            return response()->json([
+                'status' => 'success',
+                'message' => 'User registered successfully.',
+                'data' => [
+                    'user' => $user,
+                    'token' => $token,
+                    'token_type' => 'Bearer'
+                ]
+            ], 201);
+        } catch (\Exception $error) {
+            Log::error("Register Error : ", $error->getMessage());
+
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Register failed. Please try again later.'
+            ]);
+        }
     }
 
 
