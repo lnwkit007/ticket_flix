cb6a55f0fd2d94231e24f32a80534bd32f408400
lnwkit007 <164681756+lnwkit007@users.noreply.github.com>
2026-07-02 23:43:38 +0700

    Update api.

--- routes/api.php
+++ routes/api.php
@@ -10,26 +10,7 @@ use App\Http\Controllers\TicketController;
 use App\Http\Middleware\IsAdmin;
 use Illuminate\Support\Facades\Route;
 
-
-
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
-
 //////////////////////////////// No login required ////////////////////////////
-
-Route::post('/test-upload', function (Request $request) {
-
-    $file = $request->file('movie_poster');
-
-    $path = $file->storeAs(
-        'movies',
-        'test.jpg',
-        'public'
-    );
-
-    dd($path);
-});
 
 // Auth
 Route::controller(AuthController::class)->middleware('throttle:60,1')->group(function () {
