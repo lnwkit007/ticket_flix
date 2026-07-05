38feb7b6b7295f3f2e0bc462ee5bc0a9a0392e20
lnwkit007 <164681756+lnwkit007@users.noreply.github.com>
2026-07-05 23:25:49 +0700

    Update migration in user role.

--- database/migrations/2026_05_27_065752_add_role_to_users_table.php
+++ database/migrations/2026_05_27_065752_add_role_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('role')->default('customer')->after('user_email');
+            $table->enum('role', ['customer', 'admin'])->default('customer');
         });
     }
 
